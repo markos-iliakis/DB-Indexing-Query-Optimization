f55a7d584946988bfa449072caf4acefcdc896a7
Leonidas25 <leopap25@gmail.com>
2019-01-10 10:59:25 +0200

    Added join enumeration

--- join_enumeration.c
+++ join_enumeration.c
@@ -10,9 +10,18 @@ BestTree join_enumeration() {
 		while (r[i] != NULL) {
 			for (int j = 0; j < n; j++) {
 				if (r[j] != r[i]) {
-					
+					if (NoCrossProducts && !connected(r[i], r[j])) {
+						continue;
+					}
+					else {
+						BestTree ct = createJoinTree(bt(r[i]), r[j]);
+						// s' = s u r[j]
+						if (bt)
+					}
 				}
 			}
 		}
 	}
+
+	return bt;
 }
