7902c852708acbd9c928dee1689b0ca03d8f6d7e
Leonidas25 <leopap25@gmail.com>
2019-01-10 10:54:32 +0200

    Added join enumeration

--- join_enumeration.c
+++ join_enumeration.c
@@ -1,5 +1,18 @@
 BestTree join_enumeration() {
+	BestTree bt;
+	bt = createBestTree()
+
 	for (int i = 0; i <=n; i++) {
-		
+		bt(r[i]) = r[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		while (r[i] != NULL) {
+			for (int j = 0; j < n; j++) {
+				if (r[j] != r[i]) {
+					
+				}
+			}
+		}
 	}
 }
