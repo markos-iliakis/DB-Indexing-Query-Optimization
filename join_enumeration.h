c78743d2e0e69c3623553b644dcdb47d0b306e13
Leonidas25 <leopap25@gmail.com>
2019-01-16 15:37:08 +0200

    Temp stats file

---
+++ join_enumeration.h
@@ -0,0 +1,14 @@
+#ifndef JOIN_ENUMERATION_H
+#define JOIN_ENUMERATION_H
+
+
+struct BestTree;
+
+typedef struct BestTree {
+	
+} BestTree;
+
+BestTree join_enumeration();
+BestTree createJoinTree();
+//BestTree createBestTree();
+#endif
---
+++ stats.c
@@ -0,0 +1,15 @@
+int col, row;
+
+int l[col];
+int u[col];
+int f[col];
+int d[col];
+
+for (int i = 0; i < col; i++) {
+    bool* find_d;
+    int temp_l = 0;
+    int temp_u = 0;
+    int temp_f = 0;
+
+    
+}
