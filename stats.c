706f73e127c726c33636d8ed215ba99e87490c6b
Leonidas25 <leopap25@gmail.com>
2019-01-16 16:04:25 +0200

    Temp stats file

--- stats.c
+++ stats.c
@@ -1,15 +1,20 @@
-int col, row;
+for (int k = 0; k < table_num; k++) {
+    int col, row;
 
-int l[col];
-int u[col];
-int f[col];
-int d[col];
+    int l[col]; //lowest value
+    int u[col]; //largest value
+    int f[col]; //total num of values
+    int d[col]; // num of unique valuess
 
-for (int i = 0; i < col; i++) {
-    bool* find_d;
-    int temp_l = 0;
-    int temp_u = 0;
-    int temp_f = 0;
+    for (int i = 0; i < col; i++) {
+        bool* find_d;
+        int temp_l = 0;
+        int temp_u = 0;
+        int temp_f = 0;
 
-    
+        for (int j = 0; j < row; j++) {
+            temp_f++;
+
+        }
+    }
 }
