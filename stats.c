bfddfb133df6a99fb9049f7aa28f2f3afb113c62
Leonidas25 <leopap25@gmail.com>
2019-01-16 16:54:24 +0200

    Temp stats file

--- stats.c
+++ stats.c
@@ -27,18 +27,17 @@ for (int k = 0; k < table_num; k++) {
 
         bool* find_d;
 
-        //malloc find_d
+        int bool_lim = u[i] - l[i] + 1;
+        if (bool_lim > N)
+            find_d = malloc(N*sizeof(bool));
+        else
+            find_d = malloc(bool_lim*sizeof(bool));
+
+        for (int m = 0; m < bool_lim; m++)
+            find_d[m] = false;
 
         for (int j = 0; j < row; j++) {
-            temp_f++;
-
-            if (temp_l > tables[table_num][row][col]) {
-                temp_l = tables[table_num][row][col];
-            }
-
-            if (temp_u < tables[table_num][row][col]) {
-                temp_u = tables[table_num][row][col];
-            }
+            
         }
     }
 }
