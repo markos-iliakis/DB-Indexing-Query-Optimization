2a89f5d28f4de345a9d38132fee6f57dd45318ee
Leonidas25 <leopap25@gmail.com>
2019-01-16 16:31:57 +0200

    Temp stats file

--- stats.c
+++ stats.c
@@ -1,3 +1,5 @@
+#define N 50000000
+
 for (int k = 0; k < table_num; k++) {
     int col, row;
 
@@ -7,10 +9,23 @@ for (int k = 0; k < table_num; k++) {
     int d[col]; // num of unique valuess
 
     for (int i = 0; i < col; i++) {
-        bool* find_d;
         int temp_l = 0;
         int temp_u = 0;
         int temp_f = 0;
+
+        for (int j = 0; j < row; j++) {
+            temp_f++;
+
+            if (temp_l > tables[table_num][row][col]) {
+                temp_l = tables[table_num][row][col];
+            }
+
+            if (temp_u < tables[table_num][row][col]) {
+                temp_u = tables[table_num][row][col];
+            }
+        }
+
+        bool* find_d;
 
         //malloc find_d
 
