4397cf25fa65faab7f931af2352780ce977f2b94
Leonidas25 <leopap25@gmail.com>
2019-01-16 16:18:05 +0200

    Temp stats file

--- stats.c
+++ stats.c
@@ -12,9 +12,18 @@ for (int k = 0; k < table_num; k++) {
         int temp_u = 0;
         int temp_f = 0;
 
+        //malloc find_d
+
         for (int j = 0; j < row; j++) {
             temp_f++;
 
+            if (temp_l > tables[table_num][row][col]) {
+                temp_l = tables[table_num][row][col];
+            }
+
+            if (temp_u < tables[table_num][row][col]) {
+                temp_u = tables[table_num][row][col];
+            }
         }
     }
 }
