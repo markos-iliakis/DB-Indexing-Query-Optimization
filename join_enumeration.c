cd9432b9dbd5fe545e7e066d6d5f34c6e60c63b9
Leonidas25 <leopap25@gmail.com>
2019-01-18 19:23:13 +0200

    Adding stats

--- join_enumeration.c
+++ join_enumeration.c
@@ -36,3 +36,46 @@ BestTree createJoinTree() {
 BestTree createBestTree() {
 
 }
+
+void insert(char* buffer) {
+    // try to instantiate node for number
+    node* newptr = malloc(sizeof(node));
+    if (newptr == NULL)
+    {
+        return;
+    }
+
+    // make a new ponter
+    newptr->name = buffer;
+    newptr->next = NULL;
+
+    // check for empty list
+    if (first == NULL)
+    {
+        first = newptr;
+    }
+    // check for insertion at tail
+    else
+    {
+        // keep track of the previous spot in list
+        node* predptr = first;
+
+        // because we don't know how long this list is
+        // we must induce a forever loop until we find the end
+        while (true)
+        {
+            // check if it is the end of the list
+            if (predptr->next == NULL)
+            {
+                // add new node to end of list
+                predptr->next = newptr;
+
+                // break out of forever loop
+                break;
+            }
+
+            // update pointer
+            predptr = predptr->next;
+        }
+    }
+}
--- join_enumeration.h
+++ join_enumeration.h
@@ -1,14 +1,20 @@
 #ifndef JOIN_ENUMERATION_H
 #define JOIN_ENUMERATION_H
 
-
 struct BestTree;
+struct node;
+
+typedef struct node {
+    char* name;
+    struct node* next;
+} node;
 
 typedef struct BestTree {
-	
+
 } BestTree;
 
 BestTree join_enumeration();
 BestTree createJoinTree();
 //BestTree createBestTree();
 #endif
+void insert(char *);
